eaf7dcac617e9911a199d22a04e95be353d9a82c
Aleksander Novoselov <iviefistofel@gmail.com>
2021-11-20 01:51:29 +0500

    Fixes

---
+++ Assets/Scripts/PlatformDefs/DisableOnPC.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SpaceBattle.PlatformDefs
+{
+    public class DisableOnPC : MonoBehaviour
+    {
+        private void Start()
+        {
+#if UNITY_STANDALONE
+            gameObject.SetActive(false);
+#endif
+        }
+    }
+}
