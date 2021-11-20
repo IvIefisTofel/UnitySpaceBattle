527823ab511c5657358146cb615b91efe96009d2
Aleksander Novoselov <iviefistofel@gmail.com>
2021-11-20 08:29:41 +0500

    Fix animations & font sizes

---
+++ Assets/Scripts/PlatformDefs/FontSizeOnPc.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpaceBattle.PlatformDefs
+{
+    public class FontSizeOnPc : MonoBehaviour
+    {
+        public int fontSize;
+        
+        void Start()
+        {
+#if UNITY_STANDALONE
+            gameObject.GetComponent<Text>().fontSize = fontSize;
+#endif
+        }
+
+    }
+}
